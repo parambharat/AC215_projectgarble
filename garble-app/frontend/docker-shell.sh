bbf0ff00e9e79f3652c9b9d65ddd28538e419f5e
Joseph Kim <joseph.sh.kim11@gmail.com>
2021-11-08 19:04:14 -0500

    Update docker shell and env var

--- garble-app/frontend/docker-shell.sh
+++ garble-app/frontend/docker-shell.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-# Read the settings file
-source ./env.dev
+export IMAGE_NAME="project-garble-frontend"
 
 docker build -t $IMAGE_NAME -f Dockerfile.dev .
-docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3200:3000 $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3000:3000 $IMAGE_NAME
