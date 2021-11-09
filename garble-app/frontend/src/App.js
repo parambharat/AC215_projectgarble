74548e4258dfe735457f3ef15754e495acee85a8
Joseph Kim <joseph.sh.kim11@gmail.com>
2021-11-08 19:37:00 -0500

    Add Routes to app (uses v6 react-router)

--- garble-app/frontend/src/App.js
+++ garble-app/frontend/src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-
-      Hello.
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/summary" />
+          <Route path="/" />
+        </Routes>
+        Home
       </BrowserRouter>
     </div>
   );
