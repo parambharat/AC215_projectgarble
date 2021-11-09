88348a0ef5012627dfb82a69feb549e267dd3703
Joseph Kim <joseph.sh.kim11@gmail.com>
2021-11-08 20:57:05 -0500

    Create MUI theme

--- garble-app/frontend/src/App.js
+++ garble-app/frontend/src/App.js
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css';
-
 import Home from './Pages/Home';
 import Summary from './Pages/Summary';
+
+import Theme from './Theme';
+import {
+  ThemeProvider,
+} from '@mui/material/styles';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/summary" element={<Summary/>}/>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={Theme}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/summary" element={<Summary />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
